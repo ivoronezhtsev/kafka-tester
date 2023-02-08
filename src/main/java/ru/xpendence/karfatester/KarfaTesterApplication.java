59d9052c2b78ded63bfedb399b577c4935af60b3
igor <i.voronezhtsev@gmail.com>
2023-02-08 15:35:39 +0300

    remove unused import

--- src/main/java/ru/xpendence/karfatester/KarfaTesterApplication.java
+++ src/main/java/ru/xpendence/karfatester/KarfaTesterApplication.java
@@ -2,7 +2,6 @@ package ru.xpendence.karfatester;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
